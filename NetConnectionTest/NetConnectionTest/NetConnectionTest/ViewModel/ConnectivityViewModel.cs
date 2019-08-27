51558db3b9576ac98c4bdd6c967809f7133c87d5
IcedPuck <devilwardante@gmail.com>
2019-08-27 15:15:16 +1000

    Week6-Task2
    Due to, in most cases, the apps, such as your Facebook, Gmail and Twitter, will keep connecting the web server or a web interface to get or push data. Hence, we   would learn how to detect whether the current Internet connection of the smartphone is down.

---
+++ NetConnectionTest/NetConnectionTest/NetConnectionTest/ViewModel/ConnectivityViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using System.ComponentModel;
+
+namespace NetConnectionTest.ViewModel
+{
+
+    public class ConnectivityViewModel : INotifyPropertyChanged
+    {
+        string _connectionStatus = "";
+        //event triggered when connectivity changes
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string ConnectionStatus
+        {
+            get { return _connectionStatus; }
+            set
+            {
+                _connectionStatus = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ConnectionStatus"));
+            }
+        }
+
+        public ConnectivityViewModel()
+        {
+            //load current status
+            UpdateConnectionStatus();
+            //Using CrossConnectivity class makes it easier to work across different platforms
+            CrossConnectivity.Current.ConnectivityChanged += (sender, e) => { this.UpdateConnectionStatus(); };
+        }
+
+        void UpdateConnectionStatus()
+        {
+            //loop through all connectivity options available.
+            if (CrossConnectivity.Current != null && CrossConnectivity.Current.IsConnected == true)
+            {
+                var str = "";
+                foreach (ConnectionType type in CrossConnectivity.Current.ConnectionTypes)
+                    str += type.ToString() + " ";
+                ConnectionStatus = string.Format("Connected to {0}", str);
+            }
+            else
+                ConnectionStatus = "Not Connected";
+        }
+    }
+}
