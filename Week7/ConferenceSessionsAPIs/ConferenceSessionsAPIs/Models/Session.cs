045aa2872114a8c5ab276fb8535ea18f0399e2fe
IcedPuck <devilwardante@gmail.com>
2019-09-04 15:52:47 +1000

    week 7

---
+++ Week7/ConferenceSessionsAPIs/ConferenceSessionsAPIs/Models/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceSessionsAPIs.Models
+{
+    public class Session
+    {
+        public string SessionTitle { get; set; }
+        public string SessionDescription { get; set; }
+
+    }
+}
+    
